221446c4108a61311726a9f69a73ba5b4658e5a0
Srishti635 <srishtiahuja35@gmail.com>
2025-04-07 23:19:12 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,15 +4,17 @@ int main(){
     int a,b,n;
     scanf("%d %d",&a,&b);
     scanf("%c",&n);
+    int sum=a+b;
+    int sub=a-b;
+    int prod=a*b;
     if(n=='+'){
-        int sum =a+b;
         printf("%d",sum);
     }
     else if(n=='-'){
-        printf("%d",a-b);
+        printf("%d",sub);
     }
     else if(n=='*'){
-        printf("%d",a*b);
+        printf("%d",prod);
     }
     else{
         if(b==0){
