78a61eedf2f68cb0721e7b1f5aa826e6c8ecf69c
Srishti635 <srishtiahuja35@gmail.com>
2025-04-07 23:02:55 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,9 +1,9 @@
 // Your code here...
 #include<stdio.h>
 int main(){
-    int a,b,c;
-    scanf("%d %d %c",&a,&b,&c);
-    if(c==+){
+    int a,b,n;
+    scanf("%d %d %c",&a,&b,&n);
+    if(n==+){
         int sum=0;
         sum=a+b;
         printf("%d",sum);
