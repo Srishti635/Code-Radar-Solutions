9b98031264ed4052e9377280b48a9419ac0ddc07
Srishti635 <srishtiahuja35@gmail.com>
2025-04-07 23:26:48 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,12 +1,13 @@
 // Your code here...
 #include<stdio.h>
 int main(){
-    int a,b,n,sum,sub,prod;
+    int a,b,n,sum,sub,prod,div;
     scanf("%d %d",&a,&b);
     scanf("%c",&n);
     sum = 0;
     sub = 0;
     prod = 1;
+    div = 1;
     if(n=='+'){
         sum=a+b;
         printf("%d",sum);
@@ -20,7 +21,13 @@ int main(){
         printf("%d",prod);
     }
     else{
-        printf("%d",a/b);
+       if(b==0){
+        printf("%d","error");
+       }
+       else{
+        div = a/b;
+        printf("%d",div);
+        }
     }
     return 0;
 }
