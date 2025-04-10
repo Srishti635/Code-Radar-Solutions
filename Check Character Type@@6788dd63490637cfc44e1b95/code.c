b1bdade59f73df37f3440995d75566f606271636
Srishti635 <srishtiahuja35@gmail.com>
2025-04-10 23:22:52 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
     char ch;
-    scanf("%c",&ch);
+    scanf(" %c",&ch);
     if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
         switch(ch){
             case 'a': 
